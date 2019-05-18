ce21ea55dd8be66474e2a88f2e386eb57cd6e0d7
snarksliveshere <snarksliveshere@gmail.com>
2019-05-18 14:05:27 +0300

    more customize TextFormField

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 final appTheme = ThemeData(
   primarySwatch: Colors.teal,
   accentColor: Colors.tealAccent,
-  splashColor: Colors.amberAccent
+  splashColor: Colors.tealAccent
 );
 
 ThemeData getAppThemeData() {
